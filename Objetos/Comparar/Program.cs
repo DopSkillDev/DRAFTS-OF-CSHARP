11a4349b97431e27fb17121e4ef0d9cf45d01772
DaniloOPinheiro <danilopinheiro88@icloud.com>
2021-11-03 23:09:36 -0300

    subindo código

---
+++ Objetos/Comparar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Comparar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ==== Comparando Objeto ==== ");
+
+            var compararA = new Comparar("Espelho 1");
+            var compararB = new Comparar("Espelho 1");
+
+            Console.Write("O que esperamos é : ");
+            Console.WriteLine(compararA == compararB);
+            
+            Console.Write("O que realmente esperamos é : ");
+            Console.WriteLine(compararA.Espelho == compararB.Espelho);
+        }
+    }
+
+    public class Comparar
+    {
+        public string Espelho { get; set; }
+        public Comparar(string espelho)
+        {
+            Espelho = espelho;
+           
+        }
+    }
+}
