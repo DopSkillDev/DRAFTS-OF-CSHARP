2263f277f8b3aedaff5f8fa1768fb9fcae8c36af
DaniloOPinheiro <danilopinheiro88@icloud.com>
2022-01-24 22:11:21 -0300

    adicionando gitignore

---
+++ Objetos/Compara/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Compara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ==== Comparando Objeto ==== ");
+
+            var compararA = new ComparaObjeto("Espelho 1");
+            var compararB = new ComparaObjeto("Espelho 1");
+
+            Console.Write("O que esperamos é : ");
+            Console.WriteLine(compararA == compararB);
+            
+            Console.Write("O que realmente esperamos é : ");
+            Console.WriteLine(compararA.Espelho == compararB.Espelho);
+        }
+    }
+    
+    public class ComparaObjeto
+    {
+        public string Espelho { get; set; }
+        public CompararObjeto(string espelho)
+        {
+            Espelho = espelho;
+        }
+    }
+}
